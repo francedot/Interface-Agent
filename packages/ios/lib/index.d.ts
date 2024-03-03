8fd292c690157e36ba4b0e7807d0956a4970fcc0
Francesco Bonacci <francesco.bonacci@outlook.com>
2024-03-03 22:22:40 +0000

    Exclude lib

--- packages/ios/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types";
-export * from "./utils";
-export * from "./ios-agent";
-export * from "./ios-action-handler";
-export * from "./prompts/generate-code-selector";
-export * from "./prompts/planner-apps";
-export * from "./prompts/predict-next-nl-action";
