908be6fe5dd0733b90b5bc03aaf40182455bb554
Francesco Bonacci <francesco.bonacci@outlook.com>
2024-03-04 09:20:01 +0000

    Remove unused script

--- packages/ios/scripts/export-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export WDA_BUNDLE_ID=$(node -e "require('dotenv').config({ path: '/custom/path/to/.env' }); console.log(process.env.WDA_BUNDLE_ID)")
-export WDA_TEST_RUNNER_BUNDLE_ID=$(node -e "require('dotenv').config({ path: '/custom/path/to/.env' }); console.log(process.env.WDA_TEST_RUNNER_BUNDLE_ID)")
-export DEVICE_UDID=$(node -e "require('dotenv').config({ path: '/custom/path/to/.env' }); console.log(process.env.DEVICE_UDID)")
